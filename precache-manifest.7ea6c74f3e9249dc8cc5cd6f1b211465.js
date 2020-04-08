48550e0e20f34508c9c7ee23b04b4782b90a1328
Lenny <theiign@gmail.com>
2020-04-08 20:03:14 +0200

    Updates

---
+++ precache-manifest.7ea6c74f3e9249dc8cc5cd6f1b211465.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45b2a24ecbd0b0fe49534cb0c3c2bf9e",
+    "url": "/react-practice/index.html"
+  },
+  {
+    "revision": "caeb67d07de9b4c0978e",
+    "url": "/react-practice/static/css/main.1a307b61.chunk.css"
+  },
+  {
+    "revision": "f22416c008ce138264ed",
+    "url": "/react-practice/static/js/2.d16c4488.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-practice/static/js/2.d16c4488.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "caeb67d07de9b4c0978e",
+    "url": "/react-practice/static/js/main.dc016026.chunk.js"
+  },
+  {
+    "revision": "acb5652845169889c8c5",
+    "url": "/react-practice/static/js/runtime-main.35e6f0d0.js"
+  }
+]);
